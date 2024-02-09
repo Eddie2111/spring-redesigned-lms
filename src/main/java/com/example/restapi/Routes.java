321a53f6e2f020d73b673a6435c4c63cba9c9ada
Eddie2111 <asm.tareq.mahmood@g.bracu.ac.bd>
2024-02-10 04:39:50 +0600

    Feat: app init, get,post,classes init

---
+++ src/main/java/com/example/restapi/Routes.java
@@ -0,0 +1,38 @@
+package com.example.restapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.restapi.features.Student;
+
+@RestController
+public class Routes {
+
+    @GetMapping("/")
+    public String index() {
+        return "hello world";
+    }
+
+    @GetMapping("/params")
+    public String getMethodName(@RequestParam String param) {
+        return new String();
+    }
+
+    @PostMapping("/posts")
+    public ReturnProps postMethodName(@RequestBody ReturnProps entity) {
+        String name = entity.getStatus();
+        String message = entity.getMessage();
+        System.out.println(name);
+        return new ReturnProps(name, message);
+    }
+
+    @PostMapping("/student")
+    public Student postMethodName(@RequestBody Student entity) {
+        return entity;
+    }
+    
+
+}
